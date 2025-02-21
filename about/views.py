69248a449773609f86d6f4a405f662c661f6dc8c
Joe Ashton <joe@aswonvos.com>
2025-02-21 09:28:18 +0000

    add about view and update URL routing

--- about/views.py
+++ about/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(about_me):
+    return HttpResponse('This is the about view')   # This is the response that will be sent to the client
